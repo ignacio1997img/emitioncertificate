7e58e2fe2df328dda55bd9557a9397e4ef624d00
unknown <ignaci1997img>
2023-04-04 09:58:29 -0400

    si

--- resources/views/certificate/print.blade.php
+++ resources/views/certificate/print.blade.php
@@ -7,7 +7,8 @@
     <br><br><br>
     <br><br><br>
     <br><br><br>
-    <br>
+    <br><br><br>
+    
     <table width="100%">
         <tr>   
             <td style="text-align: center;  width:95%">
